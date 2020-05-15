759403a860c208b7e02a6ce32dee8ec9942df9af
Evelyn Acosta <acostaevelyn7911@gmail.com>
2020-05-15 16:09:41 -0500

    feat: displayed options, now need to put a way to select option

---
+++ src/Contact.java
@@ -0,0 +1,10 @@
+public class Contact {
+    public static void main(String[] args){
+        showOptions();
+    }
+    public static void showOptions(){
+        System.out.println("1. View contacts. \n 2. Add a new contact. \n 3. Search a contact by name. \n 4. Delete " +
+                "an existing contact. \n 5. Exit. \n Enter an option (1, 2, 3, 4, or 5): ");
+
+    }
+}
