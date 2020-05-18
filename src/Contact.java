8aa90a3517df985edc58d2d21a0a3897b62d2e8c
Sarah L Nainggolan <sarah.l.nainggolan@gmail.com>
2020-05-17 22:18:12 -0500

    sarah branch: add .gitignore

--- src/Contact.java
+++ src/Contact.java
@@ -3,6 +3,7 @@ public class Contact {
         showOptions();
     }
     public static void showOptions(){
+        System.out.println("Hey!");
         System.out.println("1. View contacts. \n 2. Add a new contact. \n 3. Search a contact by name. \n 4. Delete " +
                 "an existing contact. \n 5. Exit. \n Enter an option (1, 2, 3, 4, or 5): ");
 
